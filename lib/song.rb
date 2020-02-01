64964d3c02e98900bdd7bdfdf9c35989bafc2e3e
Julie  McManus <julieannesays@gmail.com>
2020-02-01 16:54:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,7 @@
+require 'pry'
+
 class Song
-  attr_accessor :name, :artist, :genre
+  attr_reader :name, :artist, :genre
 
   @@count = 0
   @@artists = []
@@ -9,44 +11,27 @@ class Song
     @name = name
     @artist = artist
     @genre = genre
-    @@count +=1
-    @@genres << genre
-    @@artists << artist
-  end
+    @@count += 1
+    @@artists << @artist
+    @@genres << @genre
+    end
 
   def self.count
     @@count
   end
 
   def self.artists
-    @@artists.uniq
-  end
-
-  def self.artist_count
-    artist_count = {}
-    @@artists.each do |artist|
-      if artist_count[artist]
-        artist_count[artist] += 1
-      else
-        artist_count[artist] = 1
-      end
-    end
-    artist_count
+    @@artists.uniq!
   end
 
   def self.genres
-    @@genres.uniq
+    @@genres.uniq!
   end
 
   def self.genre_count
-    genre_count = {}
+    counted = {}
     @@genres.each do |genre|
-      if genre_count[genre]
-        genre_count[genre] += 1
-      else
-        genre_count[genre] = 1
-      end
-    end
-    genre_count
+    counted << genre
+    binding.pry
   end
 end
